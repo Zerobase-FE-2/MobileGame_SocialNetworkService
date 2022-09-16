132fba545f948ebc0e2951079d78c3f8940c6dc3
woong <inwoong100@gmail.com>
2022-09-16 16:50:12 +0900

    feature: postlistpage, postpage 기본 구성

---
+++ src/components/common/Responsive.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const ResponsiveBlock = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  width: 1024px;
+  margin: 0 auto;
+
+  /**
+ * 브라우저 크기에 따른 component 생성
+ */
+
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Responsive = ({ children, ...rest }: { children: ReactNode }) => {
+  return <ResponsiveBlock {...rest}>{children}</ResponsiveBlock>;
+};
+
+export default Responsive;
---
+++ src/containers/common/HeaderContainer.tsx
@@ -0,0 +1,7 @@
+import Header from '../../components/common/Header';
+
+const HeaderContainer = () => {
+  return <Header />;
+};
+
+export default HeaderContainer;
