85701bc37352779dc0c8e6a268d1abf9ad751736
woong <inwoong100@gmail.com>
2022-09-17 18:44:19 +0900

    feature: 게시물 작성 페이지 기본 구성

--- src/components/write/Editor.tsx
+++ src/components/write/Editor.tsx
@@ -1,5 +1,95 @@
-const Editor = () => {
-  return <div>Editor</div>;
+import React, { useEffect, useRef } from 'react';
+import Quill from 'quill';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+import 'quill/dist/quill.bubble.css';
+
+const EditorBlock = styled(Responsive)`
+  padding-top: 5rem;
+  padding-bottom: 4rem;
+`;
+
+const TitleInput = styled.input`
+  font-size: 3rem;
+  outline: none;
+  padding-bottom: 0.5rem;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[4]};
+  margin-bottom: 2rem;
+  width: 100%;
+`;
+
+const QuillWrapper = styled.div`
+  .ql-editor {
+    padding: 0;
+    min-height: 320px;
+    font-size: 1.125rem;
+    line-height: 1.5;
+  }
+  .ql-editor.ql-blank::before {
+    left: 0px;
+  }
+`;
+
+const Editor = ({
+  title,
+  body,
+  onChangeField,
+}: {
+  title: string;
+  body: string;
+  onChangeField: Function;
+}) => {
+  const quillElement = useRef<HTMLDivElement>(null);
+  const quillInstance = useRef<Quill>(null);
+
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: 'bubble',
+      placeholder: '내용을 작성하세요...',
+      modules: {
+        toolbar: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['blockquote', 'code-block', 'link', 'image'],
+        ],
+      },
+    });
+
+    const quill = quillInstance.current;
+    quill.on('text-change', (delta, oldDelta, source) => {
+      if (source === 'user') {
+        onChangeField({ key: 'body', value: quill.root.innerHTML });
+      }
+    });
+  }, [onChangeField]);
+
+  const mounted = useRef(false);
+
+  useEffect(() => {
+    if (mounted.current || !quillInstance.current) return;
+    mounted.current = true;
+    quillInstance.current.root.innerHTML = body;
+  }, [body]);
+
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChangeField({ key: 'title', value: e.target.value });
+  };
+
+  return (
+    <EditorBlock>
+      <TitleInput
+        placeholder="제목을 입력하세요"
+        onChange={onChangeTitle}
+        value={title}
+      />
+      <QuillWrapper>
+        <div ref={quillElement}></div>
+      </QuillWrapper>
+    </EditorBlock>
+  );
 };
 
 export default Editor;
